1eaa87f0ed1f5fed10ba8b8158346daba479d157
Dirk Daems <dirk.daems@vito.be>
2023-05-04 13:55:53 +0200

    Add basic support for testing notebook samples

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Copernicus Data Space Ecosystem notebook samples
 
-This repository contains notebook samples for the Copernicus Data Space Ecosystem.
+This repository contains Jupyter notebook samples for the Copernicus Data Space Ecosystem.
+Notebook samples are grouped per kernel (sentinelhub, openeo and geo).
+
+## Automated testing
+If you want your sample to be automatically tested, add it to the `.tests` file. A Github action will pull the
+Jupyterlab container and automatically test the listed notebook samples.
---
+++ testsamples.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+kernel=$1
+container_id=$2
+
+tests_file="${kernel}/.tests"
+failed=false
+
+if [ -f "${tests_file}" ]; then
+  while read -r sample
+  do
+    echo "${sample}"
+    if ! docker exec "${container_id}" jupyter nbconvert --to notebook --execute "/home/jovyan/samples/${kernel}/${sample}"  --stdout --ExecutePreprocessor.kernel_name="${kernel}" --ExecutePreprocessor.timeout=600;
+    then
+      failed=true
+      echo "Sample ${sample} FAILED"
+    else
+      echo "Sample ${sample} SUCCEEDED"
+    fi
+  done < "${tests_file}"
+fi
+
+if [ $failed = false ]; then
+  exit 0
+else
+  exit 1
+fi
