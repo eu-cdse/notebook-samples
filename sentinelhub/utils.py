7d998caa5874b93a47b88813ad3c303e5e3c0723
Stefaan Lippens <stefaan.lippens@vito.be>
2023-06-15 14:35:29 +0200

    black sentinelhub

--- sentinelhub/utils.py
+++ sentinelhub/utils.py
@@ -8,7 +8,10 @@ import numpy as np
 
 
 def plot_image(
-    image: np.ndarray, factor: float = 1.0, clip_range: Optional[Tuple[float, float]] = None, **kwargs: Any
+    image: np.ndarray,
+    factor: float = 1.0,
+    clip_range: Optional[Tuple[float, float]] = None,
+    **kwargs: Any
 ) -> None:
     """Utility function for plotting RGB images."""
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 15))
