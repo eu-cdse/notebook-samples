48e1864c30af960b52e43c3a4092c272544ff4aa
Stefaan Lippens <stefaan.lippens@vito.be>
2023-06-16 11:02:05 +0200

    attempt to install additional notebook dependencies

--- .test-samples.sh
+++ .test-samples.sh
@@ -10,6 +10,9 @@ failed=false
 if [ -f "${tests_file}" ]; then
   while read -r sample
   do
+    # TODO: The Jupyterlab kernels are conda based: can we install requirements in there instead of this `pip --user` hack?
+    docker exec "${container_id}" pip install --user -r "/home/jovyan/samples/requirements.txt"
+
     echo "Running ${sample} at $(date)"
     if ! docker exec "${container_id}" jupyter nbconvert --to notebook --execute "/home/jovyan/samples/${kernel}/${sample}" \
                      --ExecutePreprocessor.kernel_name="${kernel}" --ExecutePreprocessor.timeout=3600;
