db2896a1ccbef5ac2e0b99a9d0de682e74f58af9
Max Kampen <max.kampen@sinergise.com>
2024-06-21 12:03:19 +0200

    Add sh-py library example notebooks

---
+++ sentinelhub/sentinelhub_py_examples/utils.py
@@ -0,0 +1,26 @@
+"""
+Utilities used by example notebooks
+"""
+
+from __future__ import annotations
+
+from typing import Any
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_image(
+    image: np.ndarray,
+    factor: float = 1.0,
+    clip_range: tuple[float, float] | None = None,
+    **kwargs: Any,
+) -> None:
+    """Utility function for plotting RGB images."""
+    _, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 15))
+    if clip_range is not None:
+        ax.imshow(np.clip(image * factor, *clip_range), **kwargs)
+    else:
+        ax.imshow(image * factor, **kwargs)
+    ax.set_xticks([])
+    ax.set_yticks([])
